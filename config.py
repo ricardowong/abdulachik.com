35291805fb710730a1525cfc4efd00fc9184157c
abdul hamid achik lopez <abdulachik@gmail.com>
2016-04-30 13:43:50 +0200

    idiot abdul, why would you check if its not a NoneType when you are trying to find a NoneType -_-

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Flask-User settings
 USER_APP_NAME        =   "abdul-blog"                # Used by email templates
-
+APP_NAME             =   "flask-blogify"
 # Flask settings
 SECRET_KEY =              os.getenv('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
@@ -14,6 +14,6 @@ MAIL_USERNAME =           os.getenv('MAIL_USERNAME')
 MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER')
 MAIL_SERVER =             os.getenv('MAIL_SERVER')
-MAIL_PORT =           int(os.getenv('MAIL_PORT')) if os.getenv('MAIL_PORT') is not None else 465
+MAIL_PORT =           int(os.getenv('MAIL_PORT')) if os.getenv('MAIL_PORT') is None else 465
 MAIL_USE_SSL = True
 MAIL_USE_TSL = False
