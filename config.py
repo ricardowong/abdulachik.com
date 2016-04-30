f132c74a21f8cf647d63ea89ee36a325fe72a6fe
abdul hamid achik lopez <abdulachik@gmail.com>
2016-04-30 13:34:45 +0200

    workarround for a pythonanywhere bug i found

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ MAIL_USERNAME =           os.getenv('MAIL_USERNAME')
 MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER')
 MAIL_SERVER =             os.getenv('MAIL_SERVER')
-MAIL_PORT =           int(os.getenv('MAIL_PORT'))
+MAIL_PORT =           int(os.getenv('MAIL_PORT')) if os.getenv('MAIL_PORT') is not None else 465
 MAIL_USE_SSL = True
 MAIL_USE_TSL = False
