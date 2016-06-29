e49d5713b34eb73de01b0608eae130d1f4d69b3b
abdul hamid achik lopez <abdulachik@gmail.com>
2016-06-29 18:27:59 +0200

    removed unused helper

--- app/helpers.py
+++ app/helpers.py
@@ -1,7 +1,2 @@
-from playhouse.shortcuts import model_to_dict
-
-def models_to_dict(models):
-	return [model_to_dict(model) for model in models]
-
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
